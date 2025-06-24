e53da659b658f299e2be68f0a933720912236071
Fabio Barros lima <fabi.neto.fb@gmail.com>
2025-06-24 18:26:02 -0300

    implementacao arte section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
-import { About } from './components/About';
+import { About } from './components/About'; 
+import Art from './components/Art';
 
 
 
@@ -18,6 +19,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   )
 }
